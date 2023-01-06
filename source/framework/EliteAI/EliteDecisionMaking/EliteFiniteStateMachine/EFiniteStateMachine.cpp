bc90ad35b1f55c2a6ea049a89ef9eaad16f0328a
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2023-01-06 13:04:44 +0100

    Changed comments

--- source/framework/EliteAI/EliteDecisionMaking/EliteFiniteStateMachine/EFiniteStateMachine.cpp
+++ source/framework/EliteAI/EliteDecisionMaking/EliteFiniteStateMachine/EFiniteStateMachine.cpp
@@ -61,16 +61,16 @@ Blackboard* FiniteStateMachine::GetBlackboard() const
 
 void FiniteStateMachine::ChangeState(FSMState* newState)
 {
-    //TODO 1. If currently in a state => make sure the OnExit of that state gets called
+    // If currently in a state => make sure the OnExit of that state gets called
     if (m_pCurrentState)
     {
         m_pCurrentState->OnExit(m_pBlackboard);
     }
 
-    //TODO 2. Change the current state to the new state
+    // Change the current state to the new state
     m_pCurrentState = newState;
 
-    //TODO 3. Call the OnEnter of the new state
+    // Call the OnEnter of the new state
     if (m_pCurrentState)
     {
         m_pCurrentState->OnEnter(m_pBlackboard);
